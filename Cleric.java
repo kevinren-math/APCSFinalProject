238377cecae4e618790bc88a63b35a7aa2e6100c
Jodie Hoh <jodiehoh@gmail.com>
2017-05-16 08:46:55 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Cleric.java
@@ -0,0 +1,34 @@
+public class Cleric extends Character
+{
+	private int Con; //Concentration
+	private int Int; //Craft & Spellcraft
+	private int Cha; //Diplomacy
+	private int Wis; //Heal & Profession
+	private int Arcana; //Knowledge, history, religion, the planes
+
+	public Cleric ()
+	{
+		super();
+		//implement Dice Rolling
+		
+		Con = 0;
+		
+	
+	}
+
+	public String LevelUp()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+}
