0fc8a71823bc98867cf070cfaabc512dfc2177f4
dorlokk <robodudeag@gmail.com>
2017-05-18 07:45:18 -0700

    stuff

--- MissionScript.java
+++ MissionScript.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class MissionScript extends GameScript extends TextReader
+public class MissionScript extends GameScript
 {
 	static Scanner kb;
 	public static void main(String[]args)
@@ -10,13 +10,17 @@ public class MissionScript extends GameScript extends TextReader
 	public MissionScript() 
 	{
 		super();
+
+			
+		
+	}
+	public void prompt()
+	{
 		kb = new Scanner(System.in);
 		System.out.println("You wake up at the mouth of a large cave high on a mountain. As you wake up you see a fire at the top of one of the legendary kings watchtower. The watchtowers are a mystery to everyone as no one knows who built them. You decide to go and investigate what is going on. As you walk down the mountian you hear a rustling from one of the bushes next to you. What do you do?");
 		String response = kb.nextLine();
 		if(response.getResponse() == 0)
 			System.out.println("That is not a valid response please try again");
-			
-		
 	}
 	
 	
