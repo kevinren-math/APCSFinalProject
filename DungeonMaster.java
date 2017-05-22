ddd696fd00fd5fc56061b137c75a74597e98cf9f
Jodie Hoh <jodiehoh@gmail.com>
2017-05-22 08:52:14 -0700

    DungeonMaster Script

---
+++ DungeonMaster.java
@@ -0,0 +1,16 @@
+public class DungeonMaster {
+	
+	public DungeonMaster() {
+		int wis =  8 + diceRoll(8)
+		initStat("MasterWis", wis);
+		initStat("MasterAttack", )
+		
+		
+	
+	
+	}
+
+	
+	
+	
+}
