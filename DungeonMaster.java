2d8a4fedc321a34894d6cb808eca54f2b5c8d329
Kevin Ren <kevinren99@gmail.com>
2017-05-31 08:07:55 -0700

    DungeonMaster

--- DungeonMaster.java
+++ DungeonMaster.java
@@ -1,26 +1,22 @@
 public class DungeonMaster extends Character 
 {
-		int armor;
-		int ability;
-		int heal;
-		int attack;
 	public DungeonMaster() {
 		super();
 		
 		Barbarian barbarian = new Barbarian();
-		armor = (barbarian.get("armor")) + ((barbarian.get("armor"))/2);
+		int armor = (barbarian.get("armor")) + ((barbarian.get("armor"))/2);
 		initStat("Armor", armor);
 		
 		Cleric cleric = new Cleric();
-		heal = (cleric.getHeal()) + ((cleric.getHeal())/2);
+		int heal = (cleric.getHeal()) + ((cleric.getHeal())/2);
 		initStat("Heal", heal);
 		
 		Fighter fighter = new Fighter();
-		attack = (fighter.getAttack()) + ((fighter.getAttack())/2);
+		int attack = (fighter.getAttack()) + ((fighter.getAttack())/2);
 		initStat("Attack", attack);
 		
 		Mage mage = new Mage();
-		ability = (mage.getAbility()) + ((mage.getAbility())/2) ;
+		int ability = (mage.getAbility()) + ((mage.getAbility())/2) ;
 		initStat("Ability Power", ability);
 	}
 	
